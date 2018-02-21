7d18b5aa0bbd61ffcf69a04af1d958919d2a5a2b
Ákos Szabó <akos.r9@gmail.com>
2018-02-21 22:18:09 +0100

    refactor

--- account-service/src/main/java/com/thesis/dataservice/AccountServiceApplication.java
+++ account-service/src/main/java/com/thesis/dataservice/AccountServiceApplication.java
@@ -6,9 +6,9 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
 @EnableDiscoveryClient
-public class DataServiceApplication {
+public class AccountServiceApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DataServiceApplication.class, args);
+		SpringApplication.run(AccountServiceApplication.class, args);
 	}
 }
